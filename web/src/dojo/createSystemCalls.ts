2d3cdc16bc4ed8747d27e8fbb395c9eedabd1f27
Pia <76558220+rkdud007@users.noreply.github.com>
2023-07-20 16:19:41 +0200

    madara init

--- web/src/dojo/createSystemCalls.ts
+++ web/src/dojo/createSystemCalls.ts
@@ -3,33 +3,45 @@ import { SetupNetworkResult } from "./setupNetwork";
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export enum Direction {
-    Left = 0,
-    Right = 1,
-    Up = 2,
-    Down = 3,
+  Left = 0,
+  Right = 1,
+  Up = 2,
+  Down = 3,
 }
 
-export function createSystemCalls(
-    { execute, syncWorker }: SetupNetworkResult,
-) {
-    const spawn = async () => {
-        const tx = await execute("spawn", []);
-        // await awaitStreamValue(txReduced$, (txHash) => txHash === tx.transaction_hash);
-        syncWorker.sync(tx.transaction_hash);
-        
-    };
+export function createSystemCalls({ execute, syncWorker }: SetupNetworkResult) {
+  const spawn = async () => {
+    const tx = await execute("spawn", []);
+    console.log(tx.transaction_hash);
+    console.log(await syncWorker.sync(tx.transaction_hash));
 
-    const move = async (direction: Direction) => {
-        // execute from core
-        const tx = await execute("move", [direction]);
-        // awaitStreamValue(txReduced$, (txHash) => txHash === tx.transaction_hash);
-        syncWorker.sync(tx.transaction_hash);
-        console.log("Moved ", direction)
-      };
+    let intervalId = setInterval(() => {
+      syncWorker.sync(tx.transaction_hash).then((result) => {
+        clearInterval(intervalId);
+        console.log(result);
+        return result;
+      });
+    }, 1000);
 
+    // await awaitStreamValue(txReduced$, (txHash) => txHash === tx.transaction_hash);
+    // await syncWorker.sync(tx.transaction_hash);
+  };
 
-    return {
-        spawn,
-        move
-    };
+  const move = async (direction: Direction) => {
+    // execute from core
+    const tx = await execute("move", [direction]);
+    // awaitStreamValue(txReduced$, (txHash) => txHash === tx.transaction_hash);
+
+    console.log("Moved ", direction);
+    let intervalId = setInterval(() => {
+      syncWorker.sync(tx.transaction_hash).then(() => {
+        clearInterval(intervalId);
+      });
+    }, 1000);
+  };
+
+  return {
+    spawn,
+    move,
+  };
 }
